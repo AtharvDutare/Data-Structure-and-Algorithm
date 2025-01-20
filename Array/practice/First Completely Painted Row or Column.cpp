eacbeb454319f0228f93e7b6ee76069c56e836d9
atharv <atharvdutare@gmail.com>
2025-01-20 11:03:47 +0530

    First completely Painted Row or Column done

--- Array/practice/First Completely Painted Row or Column.cpp
+++ Array/practice/First Completely Painted Row or Column.cpp
@@ -44,3 +44,33 @@ public:
         return arr.size();
     }
 };
+// my second apporach
+// Time complexity: O(n*m)
+// Space complexity: O(n+m)
+
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<int>row(m,0);
+        vector<int>col(n,0);
+        unordered_map<int,pair<int,int>> mp;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                mp[mat[i][j]]={i,j};
+            }
+        }
+        for(int i=0;i<arr.size();i++) {
+            pair<int,int> t=mp[arr[i]];
+            int x=t.first;
+            int y=t.second;
+            row[x]++;
+            col[y]++;
+            if(row[x]==n||col[y]==m) {
+                return i;
+            }
+        }
+        return arr.size();
+    }
+};
